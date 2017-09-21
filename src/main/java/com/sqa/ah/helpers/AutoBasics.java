f95e68f19fdeea937440095bef6e1033a6885094
Anne Hesemann <anne.hesemann@gmail.com>
2017-09-20 20:21:36 -0700

    Update AutoBasics

--- src/main/java/com/sqa/ah/helpers/AutoBasics.java
+++ src/main/java/com/sqa/ah/helpers/AutoBasics.java
@@ -26,6 +26,30 @@ import org.openqa.selenium.*;
  */
 public class AutoBasics {
 
+	public static List<WebElement> getByTagName(WebDriver driver, String tagName) {
+		return null;
+	}
+
+	public static List<WebElement> getCSSPropBasedElements(WebDriver driver, By locator, String prop, String value) {
+		List<WebElement> elements = driver.findElements(locator);
+		ArrayList<WebElement> filteredElements = new ArrayList<WebElement>();
+		for (int i = 0; i < elements.size(); i++) {
+			if (elements.get(i).getCssValue(prop).equalsIgnoreCase(value)) {
+				filteredElements.add(elements.get(i));
+			}
+		}
+		return filteredElements;
+	}
+
+	public static List<WebElement> getLinks(WebDriver driver) {
+		return null;
+	}
+
+	public static List<WebElement> getPictures(WebDriver driver) {
+		List<WebElement> elements = driver.findElements(By.tagName("img"));
+		return elements;
+	}
+
 	public static String getProp(String propName, String fileLocation, String fileName, Logger logger) {
 		Properties props = new Properties();
 		InputStream input;
@@ -40,6 +64,10 @@ public class AutoBasics {
 		return props.getProperty(propName);
 	}
 
+	public static List<String> getTextContents(WebDriver driver, By locator) {
+		return null;
+	}
+
 	public static boolean isElementPresent(WebDriver driver, By by, Logger logger) {
 		try {
 			WebElement element = driver.findElement(by);
@@ -47,6 +75,17 @@ public class AutoBasics {
 		} catch (NoSuchElementException e) {
 			logger.warn("Element was not found: " + by);
 			return false;
+		}
+	}
+
+	public static void logImportantImages(WebDriver driver, Logger logger) {
+		List<WebElement> images = AutoBasics.getPictures(driver);
+		for (int i = 0, j = 1; i < images.size(); i++) {
+			if (!images.get(i).getAttribute("alt").equalsIgnoreCase("")) {
+				System.out.println("#" + (j) + ": [" + images.get(i).getAttribute("alt") + "] SRC="
+						+ images.get(i).getAttribute("src"));
+				j++;
+			}
 		}
 	}
 
